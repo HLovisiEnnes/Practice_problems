3fc8e0509d3e3a8903feaea970d1e559427b4090
H Ennes <hlovisiennes@gmail.com>
2021-06-29 20:23:49 -0300

    Update Problem_4.m

--- Project Euler/Problem_4.m
+++ Project Euler/Problem_4.m
@@ -7,11 +7,22 @@ while done == 0
     if ~all(product == fliplr(product)) && c == 0
         i = i-1;
         c = 1;
-    elseif ~all(product == product(end:-1:1)) && c == 1
+    elseif ~all(product == fliplr(product)) && c == 1
         j = j-1;
         c = 0;
     else
-        done =1;
+        done = 1;
     end
 end
+
+
+for x = i:999
+    for y = j:x
+        new_product = num2str(x*y);
+        if all(new_product == fliplr(new_product))
+            product = new_product;
+        end 
+    end
+end
+
 disp(product)
